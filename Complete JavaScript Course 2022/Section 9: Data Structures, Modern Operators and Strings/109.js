0f56dbb7ba00540610dd7e11f95746c5fad0adf1
Jon Gitter <jonathan.m.gitter@gmail.com>
2022-04-11 15:41:01 -0600

    completed 109 notes

---
+++ Complete JavaScript Course 2022/Section 9: Data Structures, Modern Operators and Strings/109.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest1 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// we want to set a default number of guests for all the restaurant objects that don't have that property. We are pretending we got these restaurant objects from some kind of API and now we want to apply something to all of them. Basically adding the numGuests property to the objects that don't have them.
+
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// logical OR assignment operator
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// logical nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// logical AND assignment operator
+rest1.owner = rest1.owner && '<ANONYMOUS>';
+rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);

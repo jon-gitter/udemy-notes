9159d9663798def5d7749956043c7e18d109abc4
Jon Gitter <jonathan.m.gitter@gmail.com>
2022-04-12 16:35:23 -0600

    completed notes 112 and 113

--- Complete JavaScript Course 2022/Section 9: Data Structures, Modern Operators and Strings/113.js
+++ Complete JavaScript Course 2022/Section 9: Data Structures, Modern Operators and Strings/113.js
@@ -48,3 +48,32 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+// we want opening hours for monday
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// with optional chaining
+console.log(restaurant.openingHours.mon?.open);
+// can have multiple optional chainings. If restaurant.openingHours does not exist then the monday property will not be read and we won't get an error
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example. we want to loop over this array and then log to the console whether the restaurant is open or closed on each of the days
+const days = ['mon', 'tu', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+// methods we can use ?. to see if a method actually exists before we call it
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+// when we call a method that doesn't exist, the ?. lets us still execute code even if it doesn't exist.
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+
+console.log(users[0]?.name ?? 'User array empty');

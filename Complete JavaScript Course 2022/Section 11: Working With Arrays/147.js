ef5839f3b956e471776081757433ac7f39776ee2
Jon Gitter <jonathan.m.gitter@gmail.com>
2022-04-28 15:57:04 -0600

    completed notes for 4/28

---
+++ Complete JavaScript Course 2022/Section 11: Working With Arrays/147.js
@@ -0,0 +1,31 @@
+'use strict';
+/////// This code is utilizing the index.html ////////
+
+// best practice is to not create code in the global context, better to create a function. Better to pass data directly into a function.
+
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}</div>
+    </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html)''
+  });
+};
+displayMovements(account1.movements);
+// console.log(containerMovements.innerHTML);

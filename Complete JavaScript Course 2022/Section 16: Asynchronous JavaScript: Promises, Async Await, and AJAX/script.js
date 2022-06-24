5b4a46c410af4da9b87243ad4170ee97e32aba51
Jon Gitter <jonathan.m.gitter@gmail.com>
2022-06-24 15:06:11 -0600

    added notes

--- Complete JavaScript Course 2022/Section 16: Asynchronous JavaScript: Promises, Async Await, and AJAX/script.js
+++ Complete JavaScript Course 2022/Section 16: Asynchronous JavaScript: Promises, Async Await, and AJAX/script.js
@@ -7,18 +7,52 @@ const countriesContainer = document.querySelector('.countries');
 ////// Welcome to Callback Hell //////
 ////////////////////////////////////////////
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
 
-  request.addEventListener('load', function () {
-    console.log(this.responseText);
+//   request.addEventListener('load', function () {
+//     // console.log(this.responseText);
 
-    const [data] = JSON.parse(this.responseText);
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    const html = `
-    <article class="country">
+//     const html = `
+//     <article class="country">
+//       <img class="country__img" src="${data.flags.png}" />
+//       <div class="country__data">
+//         <h3 class="country__name">${data.name.common}</h3>
+//         <h4 class="country__region">${data.region}</h4>
+//         <p class="country__row"><span>👫</span>${(
+//           +data.population / 1000000
+//         ).toFixed(1)}</p>
+//         <p class="country__row"><span>🗣️</span>${
+//           Object.values(data.languages)[0]
+//         }</p>
+//         <p class="country__row"><span>💰</span>${
+//           Object.values(Object.values(data.currencies)[0])[0]
+//         }</p>
+//       </div>
+//     </article>`;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// getCountryData('portugal');
+// getCountryData('usa');
+// getCountryData('germany');
+
+///////////////////////
+// we need to implement a sequence of AJAX calls so we can render the first country then the country that borders it
+///////////////////////
+
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
       <img class="country__img" src="${data.flags.png}" />
       <div class="country__data">
         <h3 class="country__name">${data.name.common}</h3>
@@ -35,12 +69,60 @@ const getCountryData = function (country) {
       </div>
     </article>`;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.insertAdjacentHTML('beforeend', html);
 
-    countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    // console.log(this.responseText);
+
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbor country 2
+    const [neighbor] = data.borders;
+    // for countries without borders
+    if (!neighbor) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor}`); // we change url to alpha to allow for country codes instead of name b/c the name from the API doesn't match with the url country name we need
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      // setting different styles for neighbor countries
+      renderCountry(data2, 'neighbor');
+    });
   });
 };
 
-getCountryData('portugal');
-getCountryData('usa');
-getCountryData('germany');
+// getCountryAndNeighbour('portugal');
+getCountryAndNeighbour('usa');
+
+// Callback Hell: when we have a lot of nested callbacks in order to execute asynchronous tasks in sequence. This happens for all asynchronous tasks not which are handled by callbacks not just AJAX calls. Can be seen via a triangular shape (see below). Promises help us escape callback hell.
+// Example:
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 second passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+      setTimeout(() => {
+        console.log('4 second passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);

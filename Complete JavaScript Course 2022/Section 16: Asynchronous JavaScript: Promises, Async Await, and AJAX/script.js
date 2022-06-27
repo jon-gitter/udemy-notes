286f188f530df1186891a07e8d4ef684099dd04b
Jon Gitter <jonathan.m.gitter@gmail.com>
2022-06-27 13:18:10 -0600

    morning notes

--- Complete JavaScript Course 2022/Section 16: Asynchronous JavaScript: Promises, Async Await, and AJAX/script.js
+++ Complete JavaScript Course 2022/Section 16: Asynchronous JavaScript: Promises, Async Await, and AJAX/script.js
@@ -4,8 +4,105 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ////////////////////////////////////////////
-////// Promises and Fetch API //////
+////// Handling Rejected Promises //////
 ////////////////////////////////////////////
+
+// we will simulate when the user loses internet connection (example of a rejected promise)
+// two ways of handling rejections.  1) pass a second callback function into the then() method. For the first callback method (line16) is going to be called for a fulfilled promise, the second callback we pass will be called when the promise was rejected (line18). Handling the error is also called catching the error
+
+const getCountryData = function (country) {
+  // country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(
+      (response) => response.json(),
+      (err) => alert(err) // the error will now display as an alert and not down in the console
+    )
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbor = data[0].borders[0];
+
+      if (!neighbor) return;
+
+      // country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbor}`);
+    })
+    .then(
+      (response) => response.json(),
+      (err) => alert(err)
+    )
+    .then((data) => renderCountry(data, 'neighbor'));
+};
+
+// we want to only call the below function when a user clicks on a button
+
+btn.addEventListener('click', function () {
+  getCountryData('portugal');
+});
+
+/////////// Refactored to Handle errors globally ///////////
+
+// creating a function that will render an error
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)}</p>
+        <p class="country__row"><span>🗣️</span>${
+          Object.values(data.languages)[0]
+        }</p>
+        <p class="country__row"><span>💰</span>${
+          Object.values(Object.values(data.currencies)[0])[0]
+        }</p>
+      </div>
+    </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+
+  // countriesContainer.style.opacity = 1;
+};
+
+const getCountryDataRefactor = function (country) {
+  // country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbor = data[0].borders[0];
+
+      if (!neighbor) return;
+
+      // country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbor}`);
+    })
+    .then((response) => response.json())
+    .then((data) => renderCountry(data, 'neighbor'))
+    .catch((err) => {
+      console.error(`${err}!!!!!!`); // Will display the error to the console. we can handle all the errors no matter where they appear in the chain at the end of the chain by adding a catch method.
+      renderError(`Something went wrong!!!!! ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    }); // finally() is used no matter what happens at the end of a promise. Good example is to hide a rotating spinner you see everywhere in web apps when you load data
+};
+
+btn.addEventListener('click', function () {
+  getCountryDataRefactor('portugal');
+});
+
+// displaying an error for a country that doesn't exist
+getCountryData('dsafdas');
+
+///////////////////////////////////////////////////////////////
 
 // const getCountryData = function (country) {
 //   const request = new XMLHttpRequest();
@@ -50,75 +147,75 @@ const countriesContainer = document.querySelector('.countries');
 // we need to implement a sequence of AJAX calls so we can render the first country then the country that borders it
 ///////////////////////
 
-const renderCountry = function (data, className = '') {
-  const html = `
-    <article class="country ${className}">
-      <img class="country__img" src="${data.flags.png}" />
-      <div class="country__data">
-        <h3 class="country__name">${data.name.common}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(
-          +data.population / 1000000
-        ).toFixed(1)}</p>
-        <p class="country__row"><span>🗣️</span>${
-          Object.values(data.languages)[0]
-        }</p>
-        <p class="country__row"><span>💰</span>${
-          Object.values(Object.values(data.currencies)[0])[0]
-        }</p>
-      </div>
-    </article>`;
+// const renderCountry = function (data, className = '') {
+//   const html = `
+//     <article class="country ${className}">
+//       <img class="country__img" src="${data.flags.png}" />
+//       <div class="country__data">
+//         <h3 class="country__name">${data.name.common}</h3>
+//         <h4 class="country__region">${data.region}</h4>
+//         <p class="country__row"><span>👫</span>${(
+//           +data.population / 1000000
+//         ).toFixed(1)}</p>
+//         <p class="country__row"><span>🗣️</span>${
+//           Object.values(data.languages)[0]
+//         }</p>
+//         <p class="country__row"><span>💰</span>${
+//           Object.values(Object.values(data.currencies)[0])[0]
+//         }</p>
+//       </div>
+//     </article>`;
 
-  countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
 
-  countriesContainer.style.opacity = 1;
-};
+//   countriesContainer.style.opacity = 1;
+// };
 
-const getCountryAndNeighbour = function (country) {
-  // AJAX call country 1
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
+// const getCountryAndNeighbour = function (country) {
+//   // AJAX call country 1
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    // Render country 1
-    renderCountry(data);
+//     // Render country 1
+//     renderCountry(data);
 
-    // Get neighbor country 2
-    const [neighbor] = data.borders;
-    // for countries without borders
-    if (!neighbor) return;
+//     // Get neighbor country 2
+//     const [neighbor] = data.borders;
+//     // for countries without borders
+//     if (!neighbor) return;
 
-    // AJAX call country 2
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor}`);
-    request2.send();
+//     // AJAX call country 2
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor}`);
+//     request2.send();
 
-    request2.addEventListener('load', function () {
-      const [data2] = JSON.parse(this.responseText);
-      console.log(data2);
+//     request2.addEventListener('load', function () {
+//       const [data2] = JSON.parse(this.responseText);
+//       console.log(data2);
 
-      // setting different styles for neighbor countries
-      renderCountry(data2, 'neighbor');
-    });
-  });
-};
+//       // setting different styles for neighbor countries
+//       renderCountry(data2, 'neighbor');
+//     });
+//   });
+// };
 
-// getCountryAndNeighbour('portugal');
-getCountryAndNeighbour('usa');
+// // getCountryAndNeighbour('portugal');
+// getCountryAndNeighbour('usa');
 
-setTimeout(() => {
-  console.log('1 second passed');
-  setTimeout(() => {
-    console.log('2 second passed');
-    setTimeout(() => {
-      console.log('3 second passed');
-      setTimeout(() => {
-        console.log('4 second passed');
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 second passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
